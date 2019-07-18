fa739e35dcf4113cc1e2ea0ea275fa64ac7859de
Charlie <cchin013@ucr.edu>
2019-07-17 23:14:06 -0700

    using blade templating to clean up redundant code

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,9 @@
 {{--Type in their ucr email + generated password--}}
 {{--This should redirect to dashboard if they're already logged in--}}
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
 
-</body>
-</html>
+@section('title', 'Login')
+
+@section('content')
+    <h1>Login</h1>
+@endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,15 +1,5 @@
 {{--Here, they will put in their ucr email--}}
 {{--This should redirect to dashboard if they are already logged in--}}
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
 
-</body>
-</html>
+@section('title', 'Register')
