fc31ecf058d7f61fb1eab1d10130ae1c0f2df0f4
Charlie <cchin013@ucr.edu>
2019-07-18 19:35:46 -0700

    boilerplate code for logging in/registering/resetting password, etc..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,17 @@
-{{--Highlander Gaming, Minecraft, information, about us, register button, support, etc, basically all the breadcrumbs/navigation--}}
-@extends('layout')
+@extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
 
+                <div class="card-body">
+                    Home Page
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
