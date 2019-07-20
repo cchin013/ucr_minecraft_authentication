65400363181cf004099560021f88e6c99a475aad
Charlie <cchin013@ucr.edu>
2019-07-19 21:47:44 -0700

    created fully functional dashboard w/ controller to validate information

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         return view('dashboard');
+    }
+
+    protected function updateMinecraftUsername(Request $request)
+    {
+        $user = $request->user();
+
+        $error = [
+            'minecraft_username.unique' => "This minecraft username is already in use.",
+            'minecraft_username.required' => "Please enter a minecraft username to register to your account."
+        ];
+
+        $this->validate($request, [
+            'minecraft_username' => ['required', 'string', 'min:3', 'max: 16', 'unique:users']
+        ], $error);
+
+        $user->update([
+            'minecraft_username' => $request->minecraft_username
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,3 +3,41 @@
 @extends('layouts.app')
 
 @section('title', 'Dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <form method="POST" action="{{ route('dashboard') }}">
+                @csrf
+                <div class="form-group row">
+                    <div class="card">
+                        <div class="card-header">Tie your Minecraft username to your UCR email</div>
+                        <div class="card-title text-center px-2 pt-2 text-danger">WARNING: You can only update your username once per hour.</div>
+                        <div class="card-body">
+                            <label for="minecraft_username"
+                                   class="col-md-12 col-form-label text-md-left">{{ __('Minecraft Username') }}</label>
+                            <div class="col-md-12">
+                                <input id="minecraft_username" type="text"
+                                       class="form-control @error('minecraft_username') is-invalid @enderror"
+                                       name="minecraft_username" value="{{ old('minecraft_username') }}"
+                                       autocomplete="minecraft_username">
+                                @error('minecraft_username')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                                <div class="form-group row mb-0 mt-4">
+                                    <div class="col-md-12">
+                                        <button type="submit" class="btn btn-block btn-primary">
+                                            {{ __('Submit') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
