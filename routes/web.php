8bab0f719ea734e502fe518f1f12f4281b2f8827
Charlie <cchin013@ucr.edu>
2019-07-19 21:44:58 -0700

    added email verification requirement to routes, added new controller method

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/', 'HomeController@index');
 
-Route::get('/dashboard', 'DashboardController@index')->middleware('auth');
+Route::get('/dashboard', 'DashboardController@index')->middleware('auth')->middleware('verified')->name('dashboard');
+Route::post('/dashboard', 'DashboardController@updateMinecraftUsername')->middleware('auth')->middleware('verified')->name('dashboard');
 
 Route::get('/support', 'SupportController@index');
